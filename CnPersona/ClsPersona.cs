daf9ec0f68c899d401335501b234b3ca30280599
VivianaMuñozCastrillon <vivi84castri@gmail.com>
2023-02-28 20:25:37 -0500

    Agregar archivos de proyecto.

---
+++ CnPersona/ClsPersona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPersona
+{
+    internal class ClsPersona
+    {
+        //Declaracion de variables
+        private string nombre { get; set; }
+        private int edad { get; set; }
+
+        //Metodos
+        public void CargarDatos()
+        {
+            Console.WriteLine("Digite su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Digite su edad");
+            edad = int.Parse(Console.ReadLine());
+
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine(nombre + " " + "su edad es" + " " + edad + " " + "años");
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+        }
+    }
+}
---
+++ CnPersona/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPersona
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Nombre:Viviana Muñoz
+        /// Fecha:26/02/2023
+        /// Descripcion:Este programa solicita el nombre y edad de una persona y muestra los datos ingresados y si es mayor de edad.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //Instancia de clase
+            ClsPersona persona = new ClsPersona();
+
+
+            //llamada de metodos
+            persona.CargarDatos();
+            persona.MostrarDatos();
+
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
